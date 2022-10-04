5e00d1cad59880e0f9ea136ab76371667d593526
Novan Agung Waskito <agung.luo@gmail.com>
2022-10-05 01:57:52 +0700

    Initial Commit

---
+++ HWS-SwiftUI-Project2GuesstheFlag/HWS_SwiftUI_Project2GuesstheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_SwiftUI_Project2GuesstheFlagApp.swift
+//  HWS-SwiftUI-Project2GuesstheFlag
+//
+//  Created by Novan Agung Waskito on 05/10/22.
+//
+
+import SwiftUI
+
+@main
+struct HWS_SwiftUI_Project2GuesstheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
